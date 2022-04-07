d078c43835794c38f9861f7bb9cac5cf3cba8287
郭淑瑛 <2235917772@qq.com>
2022-04-07 23:06:07 +0800

    网页视频保存.py

--- pythonProject （pycharm）/网页视频保存.py
+++ pythonProject （pycharm）/网页视频保存.py
@@ -3,10 +3,11 @@
 # @Author : 瑛
 # @File : 网页视频保存.py
 # @Software : PyCharm
-# https://crcdn.ydstatic.com/xuetang/202204/02b46cef9f0f/bd_02b46cef9f0f_20220406195632.mp4
+# https:？？.mp4
 import requests
 
-url = "https://crcdn.ydstatic.com/xuetang/202204/f4431656d6e1/bd_f4431656d6e1_20220407195711.mp4"
+# 获取视频网页URL
+url = "https://？？.mp4"
 # 视频为二进制文件，获取content
 mp4 = requests.get(url).content
 # 写入文件
